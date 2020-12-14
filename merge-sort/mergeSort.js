d85abfc64d0275d80a4747575c06ce94faf0f8ef
Alexander Wong <code.alexanderwong@gmail.com>
2020-12-13 21:04:49 -0800

    update

--- merge-sort/mergeSort.js
+++ merge-sort/mergeSort.js
@@ -1,3 +1,28 @@
+// function merge(left, right) {
+//   const results = [];
+
+//   while (left.length && right.length) {
+//     if (left[0] < right[0]) {
+//       results.push(left.shift());
+//     } else {
+//       results.push(right.shift());
+//     }
+//   }
+//   return [...results, ...left, ...right];
+// }
+
+// function mergeSort(arr) {
+//   if (arr.length === 1) {
+//     return arr;
+//   }
+
+//   const center = Math.floor(arr.length / 2);
+//   const left = arr.slice(0, center);
+//   const right = arr.slice(center);
+
+//   return merge(mergeSort(left), mergeSort(right));
+// }
+
 function merge(left, right) {
   const results = [];
 
@@ -22,5 +47,3 @@ function mergeSort(arr) {
 
   return merge(mergeSort(left), mergeSort(right));
 }
-
-//With credit to Stephen Grider's algorithms coursse
